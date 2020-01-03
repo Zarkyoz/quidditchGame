c2fb1aadd6bb00f7cb8f93fbb33c691db2a0724c
Damien <devoti.damien@gmail.com>
2020-01-03 23:29:48 +0100

    convention correction

--- quidditch.js
+++ quidditch.js
@@ -1,37 +1,43 @@
-var main = document.querySelector('main')
-var affichage = document.createElement('article')
-main.appendChild(affichage)
-affichage.classList = "partieG"
+// VAR HTML recovery
+var main = document.querySelector('main');
 
+//VAR Build HTML
+var affichage = document.createElement('article');
+    affichage.classList = "partieG";
+main.appendChild(affichage);
 
-// var affScore = document.createElement('article')
-// main.appendChild(affScore)
-// affScore.classList = "partieD";
-
-var vifImg = document.createElement('div')
-vifImg.classList = "vifImg";
+var vifImg = document.createElement('div');
+    vifImg.classList = "vifImg";
 affichage.appendChild(vifImg);
-var vif = new Image (100, 100);
-vif.src = "image/vif.png";
-vif.classList = "vifdor"
-affichage.appendChild(vif)
 
+var vif = new Image(100, 100);
+    vif.src = "image/vif.png";
+    vif.classList = "vifdor";
+affichage.appendChild(vif);
 
-vif.addEventListener('click', vifo)
-function vifo(){
-    affichage.removeChild(vif)
-    var vifX = vifRandom(90)
-    var vifY = vifRandom(70)
+/**
+ * @event click 
+ * @param vifo
+ */
+vif.addEventListener('click', vifo);
 
-    function vifRandom(max){
+/**
+ * @function vifo
+ */
+function vifo() {
+    affichage.removeChild(vif);
+    var vifX = vifRandom(90);
+    var vifY = vifRandom(70);
+
+    function vifRandom(max) {
         return Math.floor(Math.random() * Math.floor(max));
     }
     vif.style.left = vifX + "vh";
     vif.style.top = vifY + "vh";
-    affichage.appendChild(vif)
+    affichage.appendChild(vif);
 
-    console.log(vifX)
-    console.log(vifY)
+    console.log(vifX);
+    console.log(vifY);
 
     console.log('youpie');
-    }
+}
